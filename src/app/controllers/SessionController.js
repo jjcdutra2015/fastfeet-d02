1d87d26f7f46952937804355011586ce8bf8380a
Julio Dutra <jjcdutra@gmail.com>
2020-03-29 10:20:40 -0300

    Implementação de autenticação

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,4 +1,8 @@
+import jwt from 'jsonwebtoken';
+
 import User from '../models/User';
+
+import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
@@ -10,7 +14,22 @@ class SessionController {
       return res.status(401).json({ error: 'User not found!' });
     }
 
-    return res.json({ ok: true });
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match!' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
   }
 }
 
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '0eff0728d397b2cd8970c85193c28273',
+  expiresIn: 7,
+};
